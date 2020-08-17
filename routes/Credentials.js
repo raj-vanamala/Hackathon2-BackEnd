7e5d3803410f5c000edd7d7522ff21baae5ad883
hainag16 <36581076+hainag16@users.noreply.github.com>
2020-08-17 11:25:17 +0530

    Hackathon2 Backend Code SetUp

---
+++ routes/Credentials.js
@@ -0,0 +1,95 @@
+var express = require('express');
+var router = express.Router();
+const bcrypt = require('bcrypt');
+const MongoDb = require('mongodb');
+var jwt = require('jsonwebtoken');
+const dotenv = require('dotenv').config();
+
+
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+router.post('/signUp',async function(req,res){
+
+    try {
+
+      let salt = await bcrypt.genSalt(10);
+      let hash = await bcrypt.hash(req.body.password,salt)
+      req.body.password = hash
+
+      let url = process.env.DB1;
+      let client = await MongoDb.connect(url);
+      let db = await client.db("users");
+    
+      let data = await db.collection("hack3Users").insertOne({
+
+        "email" : req.body.email,
+        "firstName" : req.body.firstName,
+        "password" : req.body.password,
+        "mobile" : req.body.mobile,
+        "status" : req.body.status,
+        "handle" : req.body.handle,
+      })
+
+      let jwtToken = await jwt.sign({email : req.body.email,firstName : req.body.firstName},process.env.JWT,{expiresIn : "1h"}) 
+      await client.close();
+      
+      let userDetails  = {
+        "email" : req.body.email,
+        "firstName" : req.body.firstName,
+        "mobile" : req.body.mobile,
+        "status" : req.body.status,
+        "handle" : req.body.handle
+      }
+      res.json({
+        "token" : jwtToken,
+        "message" : "Registration Successful",
+        "status" : "success",
+        "userDetails" : userDetails
+      })
+    
+    } catch (error) {
+      console.log(error);
+    }
+})
+
+router.post('/signIn',async function(req,res){
+
+
+  try {
+
+    let url = process.env.DB1;
+    let client = await MongoDb.connect(url);
+    let db = await client.db("users");
+
+      let user = await db.collection("hack3Users").findOne({email : req.body.email})
+
+      let result = await bcrypt.compare(req.body.password,user.password)
+      if(result === true) {
+        
+        let jwtToken = await jwt.sign({email : req.body.email,firstName : user.firstName},process.env.JWT,{expiresIn : "1h"})
+
+        res.json({
+          "token" : jwtToken,
+          "message" : "Authentication Successful",
+          "status" : "Successful",
+          "userDetails" : user
+        })
+
+      } else {
+
+        res.json({
+          message : "Password does not match",
+          "status" : "Not Successful"
+        })
+
+      }
+    await client.close();
+
+  } catch (error) {
+    console.log(error);
+  }
+})
+
+module.exports = router;
